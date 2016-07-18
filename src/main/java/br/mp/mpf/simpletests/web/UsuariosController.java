2a2e0b2b6d4da9e718823ddc4c1b763aef1f045a
andre <andre@devel.devel>
2016-07-17 21:27:15 -0300

    Mapeando inclusão, remoção e atualização

--- src/main/java/br/mp/mpf/simpletests/web/UsuariosController.java
+++ src/main/java/br/mp/mpf/simpletests/web/UsuariosController.java
@@ -1,11 +1,14 @@
 package br.mp.mpf.simpletests.web;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,6 +34,56 @@ public class UsuariosController {
     public Usuario porId(@PathVariable Long id) {
 	// TODO tratar o id inexistente
 	return usuarioService.consultarPorId(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public Map<String, Object> incluir(@RequestBody Usuario usuario) {
+	Map<String, Object> resultado = new HashMap<>();
+	try {
+	    usuario = usuarioService.incluir(usuario);
+	    resultado.put("success", true);
+	    resultado.put("mensagem", "Usuário incluído com sucesso");
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    resultado.put("success", false);
+	    resultado.put("mensagem", e.getMessage());
+	}
+	return resultado;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public Map<String, Object> excluir(@PathVariable Long id) {
+	Map<String, Object> resultado = new HashMap<>();
+	try {
+	    Usuario usuario = new Usuario();
+	    usuario.setId(id);
+	    usuarioService.excluir(usuario);
+	    resultado.put("success", true);
+	    resultado.put("mensagem", "Usuário removido com sucesso.");
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    resultado.put("success", false);
+	    resultado.put("mensagem", e.getMessage());
+	}
+	return resultado;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public Map<String, Object> alterar(@RequestBody Usuario usuario) {
+	Map<String, Object> resultado = new HashMap<>();
+	try {
+	    usuario = usuarioService.alterar(usuario);
+	    resultado.put("success", true);
+	    resultado.put("mensagem", "Usuário alterado com sucesso");
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    resultado.put("success", false);
+	    resultado.put("mensagem", e.getMessage());
+	}
+	return resultado;
     }
 
 }
