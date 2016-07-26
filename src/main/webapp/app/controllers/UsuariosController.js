498786d07426a1c9b041d094abf6fbadd15f24c7
andrethiago <andrethiagos@gmail.com>
2016-07-26 16:39:15 -0300

    Criando a exclusão de usuários.

--- src/main/webapp/app/controllers/UsuariosController.js
+++ src/main/webapp/app/controllers/UsuariosController.js
@@ -2,15 +2,25 @@ angular.module('simpleTests').controller('UsuariosController', function($scope, 
 	
 	$scope.usuarios = [];
 	$scope.mensagemSucesso = null;
+	$scope.mensagemErro = null;
 	
 	$scope.incluirUsuario = function(usuario) {
-		console.log(usuario);
 		$http.put('http://localhost:8080/simpletests/usuarios', usuario).success(function(data) {
 			$scope.usuario = {};
 			$scope.mensagemSucesso = data.mensagem;
 			carregarUsuarios();
 		});
 	};
+	
+	$scope.excluirUsuario = function(usuario) {
+		$http.delete('http://localhost:8080/simpletests/usuarios/' + usuario.id).success(function(data) {
+			$scope.mensagemSucesso = data.mensagem;
+			carregarUsuarios();
+		}).error(function(data) {
+			console.log('error')
+			$scope.mensagemErro = data.mensagem;
+		});
+	}
 	
 	var carregarUsuarios = function() {
 		$http.get('http://localhost:8080/simpletests/usuarios').success(function(data) {
