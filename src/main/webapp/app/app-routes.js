0604912e465ab9a929e8e4c7e8d5cea30668c5f5
andre <andre@devel.devel>
2016-07-27 23:50:48 -0300

    Configurando rotas adicionais e adicionando o menu ao index.html.

--- src/main/webapp/app/app-routes.js
+++ src/main/webapp/app/app-routes.js
@@ -3,6 +3,18 @@ angular.module('simpleTests').config(function($routeProvider) {
 	
 	$routeProvider.when("/projetos", {
 		templateUrl: "projetos.partial.html"
-	})
+	});
+	
+	$routeProvider.when("/usuarios", {
+		templateUrl: "usuarios.partial.html"
+	});
+	
+	$routeProvider.when("/casosDeTeste", {
+		templateUrl: "casosDeTeste.partial.html"
+	});
+	
+	$routeProvider.otherwise({
+		redirectTo: '/projetos'
+	});
 	
 });
