0a9b892d52dec013db414518765b358812e05079
andre <andre@devel.devel>
2016-07-28 01:49:55 -0300

    Deixando apenas os métodos de tratamento de erro.

--- src/main/webapp/app/interceptors/TrataErroInterceptor.js
+++ src/main/webapp/app/interceptors/TrataErroInterceptor.js
@@ -1,18 +1,23 @@
 angular.module('simpleTests').factory('TrataErroInterceptor', function($q){
 	return {
-		request: function(config) {
+		// não é obrigatório colocar todos os estados
+		/*request: function(config) {
 			console.log('passou no request');
 			return config;
-		},
+		},*/
 		requestError: function(rejection) {
+			console.log('requestError');
+			console.log(rejection);
 			return $q.reject(rejection);
 		},
-		response: function (response) {
+		/*response: function (response) {
 			console.log('passou no response');
 			console.log(response)
 			return response;
-		},
+		},*/
 		responseError: function (rejection) {
+			console.log('responseError');
+			console.log(rejection);
 			return $q.reject(rejection);
 		}
 	};
