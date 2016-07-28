953cf35ca25a2b6b58d49bb7de113aa3040a6e2f
andrethiago <andrethiagos@gmail.com>
2016-07-28 17:27:56 -0300

    Mais configurações Spring Security.

--- src/main/java/br/mp/mpf/simpletests/infra/spring/SecurityConfig.java
+++ src/main/java/br/mp/mpf/simpletests/infra/spring/SecurityConfig.java
@@ -19,7 +19,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 	super.configure(http);
-
 	http.logout();
     }
 
