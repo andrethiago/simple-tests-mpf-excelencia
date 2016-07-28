1b8f5573ccd213fb0bdc8ad629f0f81e3fd87744
andrethiago <andrethiagos@gmail.com>
2016-07-28 16:38:04 -0300

    Configuração inicial do Spring Security.

---
+++ src/main/java/br/mp/mpf/simpletests/infra/spring/SecurityConfig.java
@@ -0,0 +1,15 @@
+package br.mp.mpf.simpletests.infra.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /*
+     * @Override protected void configure(HttpSecurity http) throws Exception {
+     * // TODO Auto-generated method stub super.configure(http); }
+     */
+
+}
