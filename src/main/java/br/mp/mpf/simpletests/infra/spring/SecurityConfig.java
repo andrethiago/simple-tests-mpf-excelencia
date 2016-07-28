577c4eb2c4a54c7c1b8efc5ce20b85377067e389
andrethiago <andrethiagos@gmail.com>
2016-07-28 17:13:40 -0300

    Configurações iniciais Spring Security.

--- src/main/java/br/mp/mpf/simpletests/infra/spring/SecurityConfig.java
+++ src/main/java/br/mp/mpf/simpletests/infra/spring/SecurityConfig.java
@@ -1,15 +1,26 @@
 package br.mp.mpf.simpletests.infra.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    /*
-     * @Override protected void configure(HttpSecurity http) throws Exception {
-     * // TODO Auto-generated method stub super.configure(http); }
-     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+	super.configure(http);
+
+	http.logout();
+    }
 
 }
